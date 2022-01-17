8ff36a3ee6839ded6bb1b45d7b87f1faf219a9c8
synodical <lena0323park@naver.com>
2022-01-17 19:49:36 +0900

    create nickname

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,12 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`); // socket = connnection to server
+
+function makeMessage(type, payload) {
+    const msg = { type, payload }; // object
+    return JSON.stringify(msg); // make object into string
+}
 
 //메세지== 이벤트 받기
 socket.addEventListener("open", () => {
@@ -8,7 +14,9 @@ socket.addEventListener("open", () => {
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data, "from the server");
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -18,8 +26,15 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value)); // send msg to backend
     input.value = "";
 }
 
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
